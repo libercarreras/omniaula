f83c592b02ac96b01e69dde5933754b8a0bf1c03
Omniscient <omniscientcentral@gmail.com>
2026-04-06 22:47:09 -0300

    Add PWA install button to the user dropdown in the header

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -14,6 +14,7 @@ import {
   DropdownMenu, DropdownMenuContent, DropdownMenuItem,
   DropdownMenuSeparator, DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { InstallPWAButton } from "@/components/InstallPWAButton";
 
 export function AppLayout() {
   const isMobile = useIsMobile();
@@ -57,6 +58,8 @@ export function AppLayout() {
               <Sun className="h-4 w-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
               <Moon className="absolute h-4 w-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
             </Button>
+
+            <InstallPWAButton />
 
             <DropdownMenu>
               <DropdownMenuTrigger asChild>
